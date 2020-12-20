f44e13b2320ad6b12e3dbac2ce5e185ac7f51c65
dswatson <david.watson@oii.ox.ac.uk>
2020-12-20 12:15:40 -0500

    restructure, noise up

--- medical.R
+++ medical.R
@@ -52,7 +52,7 @@ csv <- explain(x_i, approach = c('empirical', rep('gaussian', 9)),
 # Interventional Shapley values
 isv <- explain(x_i, approach = c('empirical', rep('causal', 9)), 
                explainer = explainer, prediction_zero = phi_0,
-               ordering = list(c(1, 2), c(3, 4), c(5:10)))$dt %>%
+               ordering = list(c(1:3), c(4:7), c(8:10)))$dt %>%
   select(-none)
 
 # Plot results
@@ -73,7 +73,9 @@ rbind(msv, csv, isv) %>%
 ### RATIONAL SHAPLEY ### 
 
 # Define the subspace: same sex and age
-subspace <- df %>% filter(sex < 0, age < quantile(age, 0.25), y < median(y))
+subspace <- df %>% 
+  filter(sex > 0, age > quantile(age, 0.25), y < median(y)) %>%
+  mutate(sex = sex + rnorm(76, sd = 0.001)) # Crashes when a feature is totally invariant
 phi_0 <- mean(subspace$y)
 x_ref <- subspace %>% 
   select(-y) %>%
@@ -94,7 +96,7 @@ csv <- explain(x_i, approach = c('empirical', rep('gaussian', 9)),
 # Interventional Shapley values
 isv <- explain(x_i, approach = c('empirical', rep('causal', 9)), 
                explainer = explainer, prediction_zero = phi_0,
-               ordering = list(c(1, 2), c(3, 4), c(5:10)))$dt %>%
+               ordering = list(c(1:3), c(4:7), c(8:10)))$dt %>%
   select(-none)
 
 # Plot results
@@ -110,42 +112,6 @@ rbind(msv, csv, isv) %>%
   theme_bw() + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   facet_wrap(~ value)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-dat <- rbind(msv, csv, isv) %>%
-  mutate(value = rep(c('Marginal', 'Conditional', 'Interventional'), 
-                     each = 45)) %>%
-  mutate(value = factor(
-    value, levels = c('Marginal', 'Conditional', 'Interventional'))
-  ) %>%
-  pivot_longer(cols = -value, names_to = 'feature', values_to = 'phi')
-ggplot(dat, aes(phi, feature, fill = phi)) + 
-  geom_jitter(size = 2, width = 0, height = 0.1, color = 'black', pch = 21) + 
-  geom_vline(xintercept = 0, color = 'red', linetype = 'dashed') +
-  scale_fill_distiller('Shapley\nValue', type = 'div', palette = 'RdBu', 
-                       limits = c(-1, 1) * max(abs(dat$phi))) +
-  xlab('Shapley Value') + ylab('Feature') + 
-  theme_bw() + 
-  theme(plot.title = element_text(hjust = 0.5)) + 
-  facet_wrap(~ value)
-
-
-
-
-
 
 
 
